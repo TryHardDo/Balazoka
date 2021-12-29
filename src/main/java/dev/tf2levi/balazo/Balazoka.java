40ae6123628a6b74f313bc1905c93314dfe4b131
TryHardDo <51400985+TryHardDo@users.noreply.github.com>
2021-12-29 22:16:10 +0100

    Setting up the base of the plugin

---
+++ src/main/java/dev/tf2levi/balazo/Balazoka.java
@@ -0,0 +1,17 @@
+package dev.tf2levi.balazo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Balazoka extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
